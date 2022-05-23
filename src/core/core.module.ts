ec240a2fc62e110f8fd3b86f32fcb957f7883bfa
Connie Leung <connie.leung@diginex.com>
2022-05-23 20:16:31 +0800

    feat: initialize flagsmith nodejs client

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,18 @@
-import { Module } from '@nestjs/common';
-import { ApiConfigService } from './services/api-config.service';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ApiConfigService } from './services';
+import * as flagsmith from 'flagsmith-nodejs';
 
 @Module({
   providers: [ApiConfigService],
+  exports: [ApiConfigService],
 })
-export class CoreModule {}
+export class CoreModule implements OnModuleInit {
+  constructor(private configService: ApiConfigService) {}
+
+  onModuleInit() {
+    flagsmith.init({
+      environmentID: this.configService.featureToggleApiKey,
+      api: this.configService.featureToggleUrl,
+    });
+  }
+}
